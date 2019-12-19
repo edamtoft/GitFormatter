c161089a848e940aac2695ca4a8163cc1640f65c
Eric Damtoft <edamtoft@dealeron.com>
2019-12-18 19:53:16 -0500

    Add Hash type converter and tests

--- source/GitFormatter/Models/Hash.cs
+++ source/GitFormatter/Models/Hash.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Globalization;
 using System.Linq;
 using System.Text;
@@ -7,7 +8,7 @@ using GitFormatter.Utils;
 
 namespace GitFormatter.Models
 {
-  public readonly struct Hash : IEquatable<Hash>
+  public readonly partial struct Hash : IEquatable<Hash>
   {
     private readonly ReadOnlyMemory<byte> _bytes { get; }
 
@@ -25,61 +26,21 @@ namespace GitFormatter.Models
 
     public ReadOnlySpan<byte> Span => _bytes.Span;
 
-    public string ToHex() => string.Create(_bytes.Length * 2, _bytes, (chars, bytes) =>
-    {
-      const string alphabet = "0123456789abcdef";
-      var charIndex = 0;
-      var bufferIndex = 0;
-      while (charIndex < chars.Length)
-      {
-        var b = bytes.Span[bufferIndex];
-        chars[charIndex] = alphabet[b / 16];
-        chars[charIndex + 1] = alphabet[b % 16];
-        bufferIndex++;
-        charIndex += 2;
-      }
-    });
-
-    public override string ToString() => ToHex();
-
-    public static Hash FromHex(string hexString)
-    {
-      if (hexString is null)
-      {
-        throw new ArgumentNullException(nameof(hexString));
-      }
-
-      if (hexString.Length != HashLength * 2)
-      {
-        throw new FormatException("Hex string length must be divisible by 2");
-      }
-
-      var hexStringSpan = hexString.AsSpan();
-
-      var bytes = new byte[HashLength];
-
-      for (var i = 0; i < bytes.Length; i++)
-      {
-        var substring = new string(hexStringSpan.Slice(i * 2, 2));
-        bytes[i] = Convert.ToByte(substring, 16);
-      }
-
-      return new Hash(bytes);  
-    }
-
     public override bool Equals(object obj) => obj is Hash hash && Equals(hash);
 
-    public bool Equals(Hash other) => _bytes.Span.SequenceEqual(other._bytes.Span);
+    public bool Equals(Hash other) => Span.SequenceEqual(other.Span);
 
     public override int GetHashCode()
     {
       var hashCode = new HashCode();
-      foreach (var b in _bytes.Span)
+      foreach (var b in Span)
       {
         hashCode.Add(b);
       }
       return hashCode.ToHashCode();
     }
+
+    public override string ToString() => ToHex();
 
     public static bool operator ==(Hash left, Hash right) => left.Equals(right);
     public static bool operator !=(Hash left, Hash right) => !(left == right);
---
+++ test/GitFormatterTests/Formatting/HashObjectTests.cs
@@ -0,0 +1,32 @@
+﻿using GitFormatter.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitFormatterTests.Formatting
+{
+  [TestClass]
+  public class HashObjectTests
+  {
+    public void EqualityComparison()
+    {
+      var a = Hash.FromHex("4b825dc642cb6eb9a060e54bf8d69288fbee4904");
+      var b = Hash.FromHex("4b825dc642cb6eb9a060e54bf8d69288fbee4904");
+
+      Assert.AreEqual(a.GetHashCode(), b.GetHashCode());
+      Assert.IsTrue(a.Equals(b));
+      Assert.IsTrue(a == b);
+    }
+
+    public void NegativeEqualityComparison()
+    {
+      var a = Hash.FromHex("4b825dc642cb6eb9a060e54bf8d69288fbee4904");
+      var b = Hash.FromHex("4b825dc642cb6eb9a060e54bf8d69288fbee4905");
+
+      Assert.AreNotEqual(a.GetHashCode(), b.GetHashCode());
+      Assert.IsFalse(a.Equals(b));
+      Assert.IsFalse(a == b);
+    }
+  }
+}
